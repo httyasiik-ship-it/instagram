ddbe9c766fafb7e2ccda4f5c45e78815a0c28d5f
httyasiik-ship-it <httyasiik@gmail.com>
2026-06-11 12:28:01 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // تفعيل الصلاحيات لكي يستقبل السيرفر البيانات القادمة من ملف الـ HTML المحلي
 app.use(cors());
